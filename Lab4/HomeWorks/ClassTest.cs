db7d0c239dc81658e2d88af412fe81c6d36ca77c
flying_to_the_stars <len4yk13@mail.ru>
2021-12-16 15:04:04 +0300

    exceptions- lab3

--- Lab4/HomeWorks/ClassTest.cs
+++ Lab4/HomeWorks/ClassTest.cs
@@ -1,4 +1,5 @@
 using System;
+using NUnit.Framework;
 
 namespace Lab4.HomeWorks
 {
@@ -9,7 +10,21 @@ namespace Lab4.HomeWorks
     {
         public static void MetrodWithException(int temp) 
         {
+            switch (temp)
+            {
+                case 10: throw new Exception();
+                case 4: throw new Exception();
+                case 99: throw new Exception();
+
+            }
             
+           if (temp == 11) Assert.AreEqual(temp, 11);
+           else if(temp == 2) Assert.AreEqual(temp, 2);
+           else if (temp == 89) Assert.AreEqual(temp, 89);
+            
+
+
+
         }
     }
 }
